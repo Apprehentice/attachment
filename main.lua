9b92f274e64f052f74b2a0462442b439e5a11644
Apprehentice <Apprehentice@gmail.com>
2015-03-17 18:21:21 -0700

    Add a test file to make sure things actually work

---
+++ main.lua
@@ -0,0 +1,38 @@
+---
+-- This is a test script for the Attachment module.
+-- Expected behavior:
+--   Green text should appear on screen reading "Counter: " followed by
+--   the value of the counter variable. The counter variable should
+--   rise until it reaches 100 and stop.
+--   In addition, Cyan text should appear on screen reading "Hello, World!"
+
+local attachment = require("attachment")()
+
+local counter = 0
+local font = love.graphics.newFont(12)
+
+attachment:attach("draw", function()
+  love.graphics.setFont(font)
+  love.graphics.setColor(0, 255, 0)
+  love.graphics.print(string.format("Counter: %d", counter), 0, 0)
+end)
+
+local updateFunc = attachment:attach("update", function()
+  counter = counter + 1
+end)
+
+attachment:attach("update", function()
+  if counter >= 100 then
+    attachment:detach("update", updateFunc)
+  end
+end)
+
+local obj = {
+  draw = function()
+    love.graphics.setFont(font)
+    love.graphics.setColor(0, 255, 255)
+    love.graphics.print("Hello, World!", 100, 100)
+  end
+}
+
+attachment:attachObject(obj)
